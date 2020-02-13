f310a31c86ddc5defa2ebf2f8b3c9cbef2eb9f95
KHJcode <kimstudswcode4485@gmail.com>
2020-02-13 17:34:02 +0900

    8th Study

---
+++ Syntax/function.js
@@ -0,0 +1,18 @@
+function printA() {
+  console.log("A", "B", "C");
+}
+
+function print1() {
+  console.log(1, 2, 3);
+}
+
+print1();
+printA();
+print1();
+printA();
+print1();
+printA();
+print1();
+printA();
+print1();
+printA();
