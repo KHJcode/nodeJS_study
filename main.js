66f933051f9bd3e269766720567f245a639b4456
KHJcode <kimstudswcode4485@gmail.com>
2020-02-20 00:23:41 +0900

    9th Study

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateHTML(title, list, body){
   return `
   <!DOCTYPE html>
   <html>
   <head>
-    <title>WEB1 - ${title}</title>
+    <title>WEB2 - ${title}</title>
     <meta charset="utf-8">
   </head>
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
+    <a href="/create">create</a>
     ${body}
   </body>
   </html>
@@ -38,7 +40,6 @@ var app = http.createServer(function(request,response){
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
     var title = queryData.id;
-
     if(pathname === '/'){
       if(queryData.id === undefined){
         fs.readdir('./data', function(err, fileList){
@@ -64,6 +65,38 @@ var app = http.createServer(function(request,response){
           });
         });
       }
+    } else if(pathname === '/create'){
+      fs.readdir('./data', function(err, fileList){
+        var title = 'WEB - create';
+        var list = templateList(fileList);
+        var template = templateHTML(title, list, `
+          <form action="http://localhost:3000/create_process"
+          method="post">
+          <p><input type="text" name="title" placeholder="title"/></p>
+          <p>
+            <textarea name="description" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type="submit" />
+          </p>
+          </form>
+          `);
+        response.writeHead(200);
+        response.end(template);
+      });
+    } else if(pathname == '/create_process'){
+      var body ="";
+      request.on('data', function(data){
+        body += data;
+      });
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var title = post.title;
+        var description = post.description;
+        console.log(title);
+      });
+      response.writeHead(200);
+      response.end('success');
     }
     else {
       response.writeHead(200);
