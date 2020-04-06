9debe23bcc4e18a1d059315d5965ee927caca879
KHJcode <kimstudswcode4485@gmail.com>
2020-04-07 02:58:04 +0900

    200407

--- 4. Cookie/nodejs/cookie.js
+++ 4. Cookie/nodejs/cookie.js
@@ -1,7 +1,15 @@
 const http = require('http');
-http.createServer((request, response) =>{
-    //response.writeHead(200,{
-      //  'Set-Cookie':['yummy_cookie=choco','tasty_cookie=strawberry']
-    //});
-    response.end('Cookie!!');
+const cookie = require('cookie');
+
+http.createServer((request, response) => {
+  console.log(request.headers.cookie);
+  var cookies = {};
+  if (request.headers.cookie !== undefined) {
+    cookies = cookie.parse(request.headers.cookie);
+  }
+  console.log(cookies.yummy_cookie);
+  //response.writeHead(200,{
+    //'Set-Cookie':['yummy_cookie=choco','tasty_cookie=strawberry']
+  //});
+  response.end('Cookie!!');
 }).listen(3000);
