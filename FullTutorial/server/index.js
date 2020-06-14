2af55023f76b1de30168a312a9bdc5cae9a93cb0
= <kimstudswcode4485@gmail.com>
2020-06-14 10:33:36 +0900

    200614

--- FullTutorial/server/index.js
+++ FullTutorial/server/index.js
@@ -41,25 +41,25 @@ app.post('/api/users/register', (req, res) => {
 });
 
 app.post('/api/users/login', (req, res) => {
-    User.findOne({ email: req.body.email }, (err, userInfo) => {
-        if(!userInfo) {
-            return res.json({
-                loginSuccess: false,
-                message: "제공된 이메일에 해당하는 유저가 없습니다."
-            });
-        }
-        userInfo.comparePassword(req.body.password, (err, isMatch) => {
-            if (!isMatch) {
-                return res.json({loginSuccess: false, message: "비밀번호가 틀렸습니다"});
-            }
-            userInfo.generateToken((err, user) => {
-                if(err) return res.status(400).send(err);
-                //토큰을 쿠키에 저장
-                res.cookie("x_auth", user.token)
-                .status(200).json({ loginSuccess: true, userId: user._id });
-            });
-        });
-    });
+  User.findOne({ email: req.body.email }, (err, userInfo) => {
+      if(!userInfo) {
+          return res.json({
+              loginSuccess: false,
+              message: "제공된 이메일에 해당하는 유저가 없습니다."
+          });
+      }
+      userInfo.comparePassword(req.body.password, (err, isMatch) => {
+          if (!isMatch) {
+              return res.json({loginSuccess: false, message: "비밀번호가 틀렸습니다"});
+          }
+          userInfo.generateToken((err, user) => {
+              if(err) return res.status(400).send(err);
+              //토큰을 쿠키에 저장
+              res.cookie("x_auth", user.token)
+              .status(200).json({ loginSuccess: true, userId: user._id });
+          });
+      });
+  });
 });
 
 app.get('/api/users/auth', auth, (req, res) => {
