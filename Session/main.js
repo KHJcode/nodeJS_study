67f3fb116cfdaf394c79001db5376e0b4691bffe
KHJcode <kimstudswcode4485@gmail.com>
2020-08-12 17:12:16 +0900

    set session course

---
+++ Session/main.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const helmet = require('helmet');
+
+app.use(helmet());
+
+const indexRouter = require('./routes/index');
+const topicRouter = require('./routes/topic');
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(compression());
+app.get('*', function(request, response, next){
+  fs.readdir('./data', function(error, filelist){
+    request.list = filelist;
+    next();
+  });
+});
+
+app.use('/', indexRouter);
+app.use('/topic', topicRouter);
+
+app.use(function(req, res, next) {
+  res.status(404).send('Sorry cant find that!');
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+});
+
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!')
+});
