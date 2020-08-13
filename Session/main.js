5816067022d8441732c5df4474b7ee6140725ba8
KHJcode <kimstudswcode4485@gmail.com>
2020-08-13 19:09:55 +0900

    set login form

--- Session/main.js
+++ Session/main.js
@@ -5,16 +5,17 @@ const bodyParser = require('body-parser');
 const compression = require('compression');
 const helmet = require('helmet');
 
-app.use(helmet());
-
 const indexRouter = require('./routes/index');
 const topicRouter = require('./routes/topic');
+const authRouter = require('./routes/auth');
 
+app.use(helmet());
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
-app.get('*', function(request, response, next){
-  fs.readdir('./data', function(error, filelist){
+
+app.get('*', (request, response, next) => {
+  fs.readdir('./data', (error, filelist) => {
     request.list = filelist;
     next();
   });
@@ -22,16 +23,15 @@ app.get('*', function(request, response, next){
 
 app.use('/', indexRouter);
 app.use('/topic', topicRouter);
+app.use('/auth', authRouter);
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.status(404).send('Sorry cant find that!');
 });
 
-app.use(function (err, req, res, next) {
-  console.error(err.stack)
-  res.status(500).send('Something broke!')
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
-app.listen(3000, function() {
-  console.log('Example app listening on port 3000!')
-});
+app.listen(3000, console.log('Example app listening on port 3000!'));
