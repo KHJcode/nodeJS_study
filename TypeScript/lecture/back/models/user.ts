d8afbb03fece78f6ba2d5d72680177caf78bc07c
KHJcode <kimstudswcode4485@gmail.com>
2020-09-08 05:55:07 +0900

    login route

--- TypeScript/lecture/back/models/user.ts
+++ TypeScript/lecture/back/models/user.ts
@@ -1,14 +1,31 @@
-import { DataTypes, Model } from 'sequelize';
+import {
+  DataTypes, Model, BelongsToManyGetAssociationsMixin,
+  HasManyGetAssociationsMixin,
+  BelongsToManyRemoveAssociationMixin,
+  BelongsToManyAddAssociationMixin
+} from 'sequelize';
 import { dbType } from "./index";
+import Post from './post';
 import { sequelize } from "./sequelize";
 
-class User extends  Model {
+class User extends Model {
   public readonly id!: number;
   public nickname!: string;
   public userId!: string;
   public password!: string;
   public readonly createdAt!: Date;
   public readonly updatedAt!: Date;
+
+  public readonly Posts?: Post[];
+  public readonly Followers?: User[];
+  public readonly Followings?: User[];
+
+  public addFollowing!: BelongsToManyAddAssociationMixin<User, number>;
+  public getFollowings!: BelongsToManyGetAssociationsMixin<User>;
+  public removeFollowing!: BelongsToManyRemoveAssociationMixin<User, number>;
+  public getFollowers!: BelongsToManyGetAssociationsMixin<User>;
+  public removeFollower!: BelongsToManyRemoveAssociationMixin<User, number>;
+  public getPosts!: HasManyGetAssociationsMixin<Post>;
 }
 
 User.init({
@@ -33,7 +50,9 @@ User.init({
 });
 
 export const associate = (db: dbType) => {
-
+  db.User.hasMany(db.Post, { as: 'Posts' });
+  db.User.belongsToMany(db.User, { through: 'Follow', as: 'Followers', foreignKey: 'followingId' });
+  db.User.belongsToMany(db.User, { through: 'Follow', as: 'Followings', foreignKey: 'followerId' });
 };
 
 export default User;
